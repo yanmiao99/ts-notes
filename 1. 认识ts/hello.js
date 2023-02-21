071dd750666a5a49d16d971acfb25af85c007dcf
yanmiao99 <503084944@qq.com>
2023-02-21 21:07:54 +0800

    1. 初始ts

---
+++ 1. 认识ts/hello.js
@@ -0,0 +1,14 @@
+"use strict";
+// 1. 我在 text 形参中写了 string 类型的校验 , 所以传入的类型必须是 string ,否则报错
+const hello = (text) => {
+    document.write('hello' + text);
+};
+const a = 123;
+console.log(a);
+// 2. 我如果不传递参数或者传递一个非string 类型的值也会报错
+hello('world');
+// 3. 手动编译
+// 在终端中 运行 tec ./hello.ts 则会自动编译一个 js 文件
+// 4. 自动编译
+// 4.1 执行 tsc --init
+// 4.2 执行 tsc
